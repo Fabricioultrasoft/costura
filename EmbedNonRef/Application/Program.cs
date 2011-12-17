e29a0f3ab85bfa7657b9c44f326f53d767bd99a9
simon.cropp <simon.cropp@localhost>
2011-12-17 15:36:20 +1100

    add no ref sample

---
+++ EmbedNonRef/Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = Assembly.Load("ImplementationLibrary").GetType("Foo");
+            var instance = (IFoo) Activator.CreateInstance(type);
+            Console.WriteLine(instance.Bar());
+        }
+    }
+}
